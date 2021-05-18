9ce00848509946ee4a547bd069a6b640ac7321a1
Angelo Sérgio <70031876+AngeloSergio@users.noreply.github.com>
2021-05-18 17:56:07 -0300

    sitenovaversao

---
+++ ProjetoSite2/TelaLogin/cadastrar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div id="corpo-form-cad">
+        <h1 id="h1-cad">CADASTRAR</h1>
+        <form action="processa.php" method="POST">
+        <input type="text" placeholder="Digite seu nome">
+            <input type="email" placeholder="Digite seu email">
+            <input type="password" placeholder="Digite sua senha">
+            <input type="password" placeholder="Confirme sua senha">
+            <input type="submit" value="CADASTRAR">
+        </form>
+    </div>
+</body>
+</html>
---
+++ ProjetoSite2/TelaLogin/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div id="corpo-form">
+        <h1>ENTRAR</h1>
+        <form action="processa.php" method="POST">
+            <input type="email" placeholder="Digite seu email">
+            <input type="password" placeholder="Digite sua senha">
+            <input type="submit" value="ACESSAR">
+            <a href="cadastrar.php">Ainda não é inscrito? <strong>Cadastre-se!</strong></a>
+        </form>
+    </div>
+</body>
+</html>
